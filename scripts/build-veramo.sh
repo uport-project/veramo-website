9e3a738bacce208e86f03b334a04ad987db2b295
Italo Borssatto <italobb@gmail.com>
2022-06-01 21:15:46 -0300

    ci: CI migration to Github Actions

--- scripts/build-veramo.sh
+++ scripts/build-veramo.sh
@@ -5,5 +5,8 @@ GITREPO=https://github.com/uport-project/veramo.git
 rm -rf veramo
 git clone $GITREPO veramo
 cd veramo
-git fetch --all && git checkout main
-yarn && yarn build
+# git fetch --all && git checkout main
+git checkout italo/fix-docs
+yarn && yarn bootstrap && yarn build && yarn docs
+cd ..
+cp -fR ./veramo/docs/api ./docs/api
