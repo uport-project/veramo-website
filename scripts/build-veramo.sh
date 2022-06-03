d4cec9a82b1676e8dc8cace10cf2ea3a8fc6ea2a
Italo Borssatto <italobb@gmail.com>
2022-06-02 22:25:44 -0300

    ci: fixing API generator scripts

--- scripts/build-veramo.sh
+++ scripts/build-veramo.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 GITREPO=https://github.com/uport-project/veramo.git
 
 rm -rf veramo
@@ -9,4 +11,4 @@ cd veramo
 git checkout italo/fix-docs
 yarn && yarn bootstrap && yarn build && yarn docs
 cd ..
-cp -fR ./veramo/docs/api ./docs/api
+cp -fR ./veramo/docs/api ./docs/
