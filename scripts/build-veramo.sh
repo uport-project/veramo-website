9e9c4a1c45ddf1a2ccbbc930ed58dd796f9abc45
Italo Borssatto <italobb@gmail.com>
2022-06-06 22:13:01 -0300

    ci: temporary directory for Veramo build

--- scripts/build-veramo.sh
+++ scripts/build-veramo.sh
@@ -2,14 +2,18 @@
 
 set -e
 
+docsDir=$(pwd)
+
 GITREPO=https://github.com/uport-project/veramo.git
 
-rm -rf veramo
+revamoDir=$(mktemp -d)
+cd $revamoDir
 git clone $GITREPO veramo
 cd veramo
 # git fetch --all && git checkout main
 git fetch --all && git checkout italo/fix-docs
 yarn && yarn build && yarn docs
-cd ..
-rm -fR ./docs/api
-mv -fr ./veramo/docs/api ./docs/
+
+cd $docsDir
+rm -fR $docsDir/docs/api
+mv -fr $revamoDir/veramo/docs/api ./docs/
